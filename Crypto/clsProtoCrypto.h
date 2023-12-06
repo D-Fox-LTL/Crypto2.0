e39b5c4337eb5248176404a4c968c193a8ad62c3
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2023-12-06 15:57:28 +0100

    new vers

---
+++ Crypto/clsProtoCrypto.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string.h>
+#include <stdio.h>
+
+#define MaxLen 1024
+#define CryptKey 8
+
+
+
+//abstraktní tøída je tøída, která má zakompovanou nìjakou abstraktní fce			nelze udìlat objekt
+class clsProtoCrypto
+{
+public:
+	//konstruktor
+	clsProtoCrypto() {};
+
+	//open file
+	/*virtual*/ bool OpenFile(const char* p_chNameSource, const char* p_chNameDest) /*= 0*/ { return false; };		//virtual - bude nahrazena			...=0, tøída je abstraktní
+
+	//close file
+	virtual bool CloseFile() = 0;
+
+	// Crypt file
+	virtual bool Crypt() = 0;
+
+	// Decrypt file
+	virtual bool Decrypt() = 0;
+};
+
